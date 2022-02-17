0d0634568da0a08cea5d4948d11742bd32f7025a
Luiz Cirilo Tomasi Neto <65315838+Luiz-Tomasi@users.noreply.github.com>
2022-02-17 14:29:34 -0300

    Corrigindo erro de português nos comentários

--- Questões/Desafio1.java
+++ Questões/Desafio1.java
@@ -9,7 +9,7 @@ class Main {
     System.out.print("Quantos degraus deve ter a escada: ");
     int n = scan.nextInt();
       for (int i = 1; i<=n; i++){    // O primeiro for() funciona para botar os degraus da escada
-        for (int j = i; j<=n; j++){  // Já o segundo for() coloca os espaçõs antes de cada degrau
+        for (int j = i; j<=n; j++){  // Já o segundo for() coloca os espaços antes de cada degrau
         System.out.print(" ");       // Assim criando 2 triangulos um oposto ao outro, um com espaçõs vazios
         }                            // e o outro com * formando a escada
         for (int j = 1; j<=i; j++){
